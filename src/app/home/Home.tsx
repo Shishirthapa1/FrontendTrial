3717d32dccfa30ebe36b8d9b7de9ee3b0cc88d60
Shishirthapa1 <sisirjangthapa@gmail.com>
2025-09-07 13:30:37 +0545

    Initial Setup

---
+++ src/app/home/Home.tsx
@@ -0,0 +1,14 @@
+import Navbar from '@/Components/Navbar';
+import Topbar from '@/Components/Topbar';
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div>
+            <Topbar />
+            <Navbar />
+        </div>
+    )
+}
+
+export default HomePage;
